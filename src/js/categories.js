51f520e229e55216512f60a62d65b9da34a396c0
Illia23 <Priymak2331@gmail.com>
2023-10-21 02:09:06 +0200

    скоротив код

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,101 +1,41 @@
-const btnCategories = document.querySelectorAll(".btn-categories");
 const containerForImages = document.querySelector('.js-container-for-images');
 const bodyPartsFilterButton = document.getElementById('bodyPartsFilterButton');
-// Зробіть кнопку "Body parts" активною
 
-bodyPartsFilterButton.classList.add('active');
-
-// Отримайте категорії для фільтру "Body parts" і вставте їх у блок
-fetch('https://your-energy.b.goit.study/api/filters?filter=Body parts')
-  .then(response => response.json())
-    .then(data => {
-        let results = data.results;
-        let resultsHtml = results.map(({ filter, imgURL, name }) => {
-        return  ` <div class="gallery__item">
-            <a class="gallery__link" href="#">
-                <img
-                class="gallery__image"
-                src="${imgURL   }"
-                alt="${name}"
-                />
-            </a>
-            <p class="textImage">${name}</p>
-            <p class="filterImage">${filter}</p>
-            </div>` ;
-            
-
-   }).join('');
-    containerForImages.insertAdjacentHTML('beforeend', resultsHtml);
-
-    
-  })
-  .catch(error => {
-    console.error('Помилка при отриманні категорій: ', error);
-  });
-
-// Define the click event handler function.
-function handleClick(event) {
-
-    // Define the URL of the backend API or server. 
-    const apiUrl = new URL('https://your-energy.b.goit.study/api/filters');
-    const params = new URLSearchParams();
-
-    const element = event.target;
-    const categoryFilterValue = element.dataset.cgid;
-    
-    params.append('filter', categoryFilterValue);
-    params.append('page', 1);
-    params.append('limit', 12);
-
-    apiUrl.search = params.toString();
-
-    // Make a GET request to the backend. 
-    fetch(apiUrl) 
-    .then(response => { 
-        // Check if the response status indicates success (e.g., 200 OK). 
-        if (response.status === 200) { 
-        // Parse the response data as JSON. 
-        return response.json(); 
-        } else { 
-        // Handle non-successful response (e.g., display an error message). 
-        throw new Error('Request failed'); 
-        } 
-    }) 
-    .then(data => { 
-        // Process the data received from the backend. 
-        console.log(data);
-        handleRespons(data); // You can do something with the data here.
-    }) 
-    .catch(error => { 
-        // Handle any errors that occurred during the request. 
-        console.error('Error:', error); 
-    });
+// Отримати категорії та вставити їх у блок
+function fetchCategories(filterValue) {
+  fetch(`https://your-energy.b.goit.study/api/filters?filter=${filterValue}`)
+    .then(response => response.json())
+    .then(data => handleResponse(data))
+    .catch(error => console.error('Помилка при отриманні категорій: ', error));
 }
 
-// Add the click event to each selected element.
-btnCategories.forEach(function (element) {
-  element.addEventListener('click', handleClick);
-});
-
-function handleRespons(data) {
-    let results = data.results;
-   let resultsHtml = results.map(({ filter, imgURL, name }) => {
-      return  ` <div class="gallery__item">
+function handleResponse(data) {
+  const resultsHtml = data.results.map(({ filter, imgURL, name }) => {
+    return  ` <div class="gallery__item">
         <a class="gallery__link" href="#">
             <img
             class="gallery__image"
-            src="${imgURL   }"
+            src="${imgURL}"
             alt="${name}"
             />
         </a>
         <p class="textImage">${name}</p>
         <p class="filterImage">${filter}</p>
-        </div>` ;
-        
+        </div>`;
+  }).join('');
 
-   }).join('');
-    containerForImages.innerHTML = '';
-    containerForImages.insertAdjacentHTML('beforeend', resultsHtml);
-
+  containerForImages.innerHTML = resultsHtml;
 }
 
+// Зробити кнопку "Body parts" активною та завантажити категорії
+bodyPartsFilterButton.classList.add('active');
+fetchCategories('Body parts');
+
+// Опціонально: додати обробник подій для інших категорій
+const btnCategories = document.querySelectorAll(".btn-categories");
+btnCategories.forEach(element => {
+  element.addEventListener('click', event => {
+    const categoryFilterValue = event.target.dataset.cgid;
+    fetchCategories(categoryFilterValue);
+  });
+});
