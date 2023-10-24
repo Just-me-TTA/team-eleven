4d44a799045ea82ccf993b7d30fe7f8ffb0d02df
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2023-10-24 09:06:53 +0300

    update js file

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,18 +1,31 @@
 const text = document.querySelector('.js-quote');
-serviceQuote().then(data => {
-  text.innerHTML = `
-    <p class="js-quote-text">${data.quote}</p>
-    <p class="js-quote-autor">${data.author}</p>
-     `;
-})
 
-function serviceQuote() {
+async function serviceQuote() {
   const BASE_URL = "https://your-energy.b.goit.study/api/quote";
-  return fetch(
-        `${BASE_URL}`).then((resp) => {
-          if (!resp.ok) {
-            throw new Error(resp.statusText);
-          }
-          return resp.json();
-        })
+  try {
+      const response = await fetch(`${BASE_URL}`);
+      const data = response.data;
+      serviceQuote().then(data => {
+        text.innerHTML = `
+        <p class="js-quote-text">${data.quote}</p>
+        <p class="js-quote-autor">${data.author}</p>
+        `;
+        if (data) {
+          const day = new Date();
+          localStorage.setItem('quote', data.quote);
+          localStorage.setItem('author', data.author);
+          localStorage.setItem('quoteDate', day);
+        }
+      })
+
+      return response.json();
+  } catch (error) {
+     console.log(error)
+  }
+
 }
+
+serviceQuote()
+
+
+
