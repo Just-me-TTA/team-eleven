a37dd37417c79ca79e3875781a7ba35c1c854e2e
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2023-10-25 20:52:19 +0300

    Update quote.js

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,31 +1,27 @@
 const text = document.querySelector('.js-quote');
+const KEY_LOCALSTORAGE = 'quote';
 
 async function serviceQuote() {
   const BASE_URL = "https://your-energy.b.goit.study/api/quote";
   try {
-      const response = await fetch(`${BASE_URL}`);
-      const data = response.data;
-      serviceQuote().then(data => {
-        text.innerHTML = `
+    const response = await fetch(BASE_URL);
+    const data = await response.json();
+
+    if (data) {
+      const text = document.querySelector('.js-quote');
+      text.innerHTML = `
         <p class="js-quote-text">${data.quote}</p>
         <p class="js-quote-autor">${data.author}</p>
-        `;
-        if (data) {
-          const day = new Date();
-          localStorage.setItem('quote', data.quote);
-          localStorage.setItem('author', data.author);
-          localStorage.setItem('quoteDate', day);
-        }
-      })
+      `;
 
-      return response.json();
+      const day = new Date();
+      localStorage.setItem('quote', data.quote);
+      localStorage.setItem('author', data.author);
+      localStorage.setItem('quoteDate', day);
+    }
   } catch (error) {
-     console.log(error)
+    console.error(error);
   }
-
 }
 
-serviceQuote()
-
-
-
+serviceQuote();
