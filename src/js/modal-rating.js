870cb8e75ad0a9e69629769619236bbd91cb91dd
sofiatuzinska <129293949+sofiatuzinska@users.noreply.github.com>
2023-10-27 17:58:16 +0300

    update

--- src/js/modal-rating.js
+++ src/js/modal-rating.js
@@ -93,31 +93,110 @@ function closeModalInput() {
 
 
 
-const refs = {
-form: document.querySelector('.js-form'),
-};
 
-refs.form.addEventListener('submit', handleSubmit);
+// const refs = {
+// form: document.querySelector('.js-form'),
+// };
+// let exerciseId = [];
 
-function handleSubmit (evt) {
-evt.preventDefault();
 
-console.log(evt.currentTarget.elements)
- const { email, comment,} = evt.currentTarget.elements
+// refs.form.addEventListener('submit', handleSubmit);
 
- const userData = {
-    email:  email.value,
-    comment: comment.value,
-    // rating: rating.value,
- };
 
- serviceQuest(userData).then(() => alert("Success!")).catch(() => alert("Oops! Check the information"))
+// function loadFavoritesFromLocalStorage() {
+//   const favoriteData = localStorage.getItem('LS_FAVORITES_ID');
+//   if (favoriteData) {
+//     exerciseId = JSON.parse(favoriteData);
+//   } else {
+//     exerciseId = [];
+//   }
+// }
+
+// function handleSubmit (evt) {
+// evt.preventDefault();
+
+// loadFavoritesFromLocalStorage()
+
+//  const { email, comment,} = evt.currentTarget.elements
+
+//  const userData = {
+//     email:  email.value,
+//     comment: comment.value,
+//     exerciseId: exerciseId,
+//     // rating: rating.value,
+//  };
+
+//  serviceQuest(userData).then(() => alert("Success!")).catch(() => alert("Oops! Check the information"))
  
-}
+// }
 
 
-function serviceQuest(data) {
-    return axios.post(`https://your-energy.b.goit.study/api/exercises/${id}/rating`, data);
-   }
+
+// function serviceQuest(data, exerciseId) {
+//     return axios.post(`https://your-energy.b.goit.study/api/exercises/${exerciseId}/rating`, data);
+// }
 
 
+
+
+////////////////////////////////////////////////////
+const refs = {
+    form: document.querySelector('.js-form'),
+  };
+  
+  let exerciseId = [];
+  
+  refs.form.addEventListener('submit', handleSubmit);
+  
+  function loadFavoritesFromLocalStorage() {
+    const favoriteData = localStorage.getItem('LS_FAVORITES_ID');
+    if (favoriteData) {
+      exerciseId = JSON.parse(favoriteData);
+    } else {
+      exerciseId = [];
+    }
+  }
+  
+  function handleSubmit(evt) {
+    evt.preventDefault();
+  
+    loadFavoritesFromLocalStorage();
+  
+    const { email, comment } = evt.currentTarget.elements;
+  
+    const userData = {
+      email: email.value,
+      comment: comment.value,
+    };
+  
+    serviceQuest(userData, exerciseId)
+      .then(() => alert('Success!'))
+      .catch(() => alert('Oops! Check the information'));
+  }
+  
+  function serviceQuest(data, exerciseIds) {
+    const url = 'https://your-energy.b.goit.study/api/exercises/rating';
+  
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data, exerciseIds }),
+    };
+  
+    return fetch(url, options)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Network response was not ok');
+        }
+      })
+      .then((responseData) => {
+        console.log(responseData); // Handle the response data as needed
+      })
+      .catch((error) => {
+        console.error(error); // Handle the error
+      });
+  }
